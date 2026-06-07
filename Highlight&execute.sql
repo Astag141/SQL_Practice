c45d98a1f311a71b0b9b1612391ea599efbaf9aa
Deviprasad shetty <devi18shetty@gmail.com>
2026-06-07 16:51:53 +0530

    code

---
+++ Highlight&execute.sql
@@ -0,0 +1,8 @@
+-- used the execute only the part of the query
+-- Highlight the part of the query you want to execute and then run it
+
+SELECT *
+FROM customers
+where country = 'USA'
+
+SELECT * FROM orders
