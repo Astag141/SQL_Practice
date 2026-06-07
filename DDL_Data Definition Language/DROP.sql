ab9e5c42e2a2543ce401d79dc6b1f5f3bbcd74fa
Deviprasad shetty <devi18shetty@gmail.com>
2026-06-07 17:24:15 +0530

    CODE

---
+++ DDL_Data Definition Language/DROP.sql
@@ -0,0 +1,6 @@
+-- DROP
+--Delete the table person fron DB
+
+DROP TABLE person
+
+--its used to remobe tables or data fron the database permanently. Once you drop a table, all the data in that table is lost and cannot be recovered unless you have a backup. Be cautious when using the DROP statement, as it can lead to data loss if not used properly.
